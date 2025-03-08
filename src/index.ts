56e7c3f7e64478ef771b0b00342f349188c6647c
Ali Al Yaman <alialyaman750@gmail.com>
2025-03-08 12:10:35 +0200

    Added parses

--- src/index.ts
+++ src/index.ts
@@ -1,52 +1,14 @@
-//SOLID principles
-//SRP - Single Responsibility Principle
-//OCP - Open/Closed Principle
-//LSP - Liskov Substitution Principle
-//ISP - Interface Segregation Principle
-//DIP - Dependency Inversion Principle
-
-import { FinanceCalculator, OrderManagement, Validator } from "./app";
 import logger from "./utils/logger";
+import { readCSVFile } from "./utils/parser";
 
 
-
-let orderManagement = new OrderManagement(
-    new Validator(),
-    new FinanceCalculator()
-  );
-
-const orders  = [
-    { id: 1, item: "Sponge", price: 15 },
-    { id: 2, item: "Chocolate", price: 20 },
-    { id: 3, item: "Fruit", price: 18 },
-    { id: 4, item: "Red Velvet", price: 25 },
-    { id: 5, item: "Coffee", price: 8 },
-  ];
+const main = async() => {
+  const data = await readCSVFile('src/data/bakery.csv');
+  data.forEach((row) => {
+    logger.info(row)
+  })
+  
+}
 
 
-orders.forEach((order) => orderManagement.addOrder(order.item, order.price));
-
-
-const newItem = "Marble";
-const newPrice = 22;
-
-
-orderManagement.addOrder(newItem, newPrice);
-
-
-logger.info("All orders: %o" , orderManagement.getOrders());
-
-
-const fetchId = 2;
-const fetchedOrder = orderManagement.getOrderById(fetchId);
-logger.info("Order with ID 2: %o" , fetchedOrder);
-
-const nonExistentId = 10;
-const nonExistentOrder = orderManagement.getOrderById(nonExistentId);
-logger.info("Order with ID 10 (non-existent): %o", nonExistentOrder);
-
-const totalRevenue = orderManagement.getRevenue();
-const avgBuyPower = orderManagement.getAvgBuyingPower();
-
-logger.info("Total revenue: %d", totalRevenue);
-logger.info("Average buying power: %d", avgBuyPower);
+main();
