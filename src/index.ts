de5a73770366b92ee92f6d33e2416d03beafa240
Ali Al Yaman <alialyaman750@gmail.com>
2025-03-14 23:35:56 +0200

    Added Cake Builder

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,29 @@
-import logger from "./utils/logger";
-import { readCSVFile } from "./utils/parser";
+import { CakeBuilder } from "./model/builders/Cake.builder";
+
+
 
 
 const main = async() => {
-  const data = await readCSVFile('src/data/cake orders.csv');
-  data.forEach((row) => {
-    logger.info(row)
-  })
+  const cakeBuilder = new CakeBuilder();
+  cakeBuilder.setType("Sponge")
+  cakeBuilder.setFlavor("Vanilla")
+  cakeBuilder.setFilling("Cream")
+  cakeBuilder.setSize(20)
+  cakeBuilder.setLayers(2)
+  cakeBuilder.setFrostingType("Buttercream")
+  cakeBuilder.setFrostingFlavor("Vanilla")
+  cakeBuilder.setDecorationType("Sprinkles")
+  cakeBuilder.setDecorationColor("Multi-color")
+  cakeBuilder.setCustomMessage("Happy Birthday")
+  cakeBuilder.setShape("Round")
+  cakeBuilder.setAllergies("Nut-Free")
+  cakeBuilder.setSpecialIngredients("Organic Ingredients")
+  cakeBuilder.setPackagingType("Standard Box")
+  cakeBuilder.setPrice(50)
+  cakeBuilder.setQuantity(1);
+  const cake = cakeBuilder.build();
   
+  console.log(cake);
 }
 
 
